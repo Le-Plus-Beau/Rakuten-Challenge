4ea0d64a2f970990a5d8fd524a048bbad4ed860e
Le_Plus_Beau <anthonysaindoy@outlook.fr>
2026-05-17 22:56:14 +0200

    je sais meme plus

--- promote_model.py
+++ promote_model.py
@@ -5,7 +5,7 @@ client = MlflowClient()
 client.set_registered_model_alias(
     name="rakuten_model",
     alias="prod",
-    version=7
+    version=8
 )
 
 print("✅ Model assigned to alias 'prod'")
